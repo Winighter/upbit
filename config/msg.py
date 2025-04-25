ed7674b0619a268696b325763bdcff4795ece84f
Zero Kim <103815045+Winighter@users.noreply.github.com>
2025-04-25 22:40:29 +0900

    Update msg.py

--- config/msg.py
+++ config/msg.py
@@ -1,6 +1,6 @@
 import datetime, requests
 
-url = 'https://discord.com/api/webhooks/1349690403829448725/7snHkoLZftpXa58O1KVS1NzhUoN9hBm2QUIwxAMcTFIHkEESIO8uHS-yFk3QPuqNcpPq'
+url = 'dicord_web_hook_url'
 
 class Message():
 
